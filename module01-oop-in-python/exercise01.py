b22611f3152307ab14426145306955f1343e0f5a
Binnur KURT <deepcloudlabs@gmail.com>
2025-06-23 13:48:51 +0300

    Update exercise01.py

--- module01-oop-in-python/exercise01.py
+++ module01-oop-in-python/exercise01.py
@@ -1,6 +1,20 @@
 """
   Logistics -> Vehicle
 """
+from enum import Enum
+
+
+class Color(Enum):
+    RED = 1
+    BLUE = 2
+    YELLOW = 3
+    GREEN = 4
+    BLACK = 5
+    WHITE = 6
+    PINK = 7
+    ORANGE = 8
+    BROWN = 9
+    GRAY = 10
 
 
 class OverCapacityError(Exception):
@@ -8,9 +22,17 @@ class OverCapacityError(Exception):
     OverCapacityError: Sub-class/Derived-Class
     Exception -> Super-class/Base class for all exceptions in this module.
     """
-    def __init__(self, message, over_weight):
+
+    def __init__(self, message: str, over_weight: float):
         super().__init__(message)
-        self.over_weight = over_weight
+        self.__over_weight = over_weight
+
+    @property
+    def over_weight(self) -> float:
+        return self.__over_weight
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}: over_weight={self.over_weight} kg."
 
 
 class Vehicle(object):
@@ -23,32 +45,31 @@ class Vehicle(object):
     """
 
     # constructor -> object -> Heap [fields]
-    def __init__(self, license_plate, capacity, current_load, color):
+    def __init__(self, license_plate: str, capacity: float = 5_000, current_load: float = 0.0,
+                 color: Color = Color.RED):
         self.__license_plate = license_plate
         self.__capacity = capacity
         self.__current_load = current_load
         self.__color = color
 
     @property
-    def current_load(self):
+    def current_load(self) -> float:
         return self.__current_load
 
     @property
-    def capacity(self):
+    def capacity(self) -> float:
         return self.__capacity
 
     @property
-    def color(self):
+    def color(self) -> Color:
         return self.__color
 
     @color.setter
-    def color(self, new_color):
+    def color(self, new_color: Color) -> None:
         # validation/business rule/invariants/policy/regulations/...
         self.__color = new_color
 
-
-
-    def load(self, weight):
+    def load(self, weight: float) -> float:
         """
         business method: loads weight to the vehicle
         :param weight: in kg and must be positive
@@ -56,16 +77,16 @@ class Vehicle(object):
         """
         # validation
         if weight <= 0:
-            raise ValueError(f"weight ({weight}) must be positive number.")
+            raise ValueError(f"weight ({weight}) must be positive number")
         # business rule/policy/regulation
         if self.__current_load + weight > self.__capacity:
-            message = f"Vehicle {self.__license_plate} is over capacity."
+            message = f"Vehicle {self.__license_plate} is over capacity"
             over_weight = self.capacity - self.__current_load - weight
             raise OverCapacityError(message, over_weight)
         self.__current_load += weight
         return self.__current_load
 
-    def unload(self, weight):
+    def unload(self, weight: float) -> float:
         """
         business method: unloads weight from the vehicle
         :param weight: in kg and must be positive
@@ -73,29 +94,29 @@ class Vehicle(object):
         """
         # validation
         if weight <= 0:
-            raise ValueError(f"weight ({weight}) must be positive number.")
+            raise ValueError(f"weight ({weight}) must be positive number")
         # validation
         if weight > self.current_load:
-            raise ValueError(f"weight ({weight}) must be less than or equal to the current load({self.current_load}).")
+            raise ValueError(f"weight ({weight}) must be less than or equal to the current load({self.current_load})")
         self.__current_load -= weight
         return self.__current_load
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Vehicle: {self.__license_plate}, {self.__color}, {self.__current_load} kg."
 
 
 try:
-    vehicle1 = Vehicle("32ABC42", 10_000, 0, "red")
-    vehicle2 = Vehicle("32ABC43", 8_000, 0, "blue")
-    vehicle1.load(2_000) # load(self=vehicle1, weight=2_000)
-    Vehicle.load(self=vehicle2,weight=3_000) # vehicle2.load(3_000)
-    print(f"vehicle2's current load is {vehicle2.current_load} kg.") # read
+    vehicle1: Vehicle = Vehicle("32ABC42", 10_000, 0, Color.GRAY)
+    vehicle2: Vehicle = Vehicle("32ABC43", 8_000, 0, Color.BLUE)
+    vehicle1.load(2_000)  # load(self=vehicle1, weight=2_000)
+    Vehicle.load(self=vehicle2, weight=3_000)  # vehicle2.load(3_000)
+    print(f"vehicle2's current load is {vehicle2.current_load} kg.")  # read
     vehicle2.load(3_000)
     vehicle1.load(4_000)
-    #vehicle2.load(4_000)
+    vehicle2.load(4_000)
     vehicle1.load(4_000)
-    #vehicle2.load(2_000)
-    vehicle1.color = "white" # write
+    # vehicle2.load(2_000)
+    vehicle1.color = Color.WHITE  # write
     print(vehicle1)
 except OverCapacityError as oce:
     print(oce)
@@ -104,5 +125,5 @@ except ValueError as ve:
 finally:
     vehicle1.unload(vehicle1.current_load)
     vehicle2.unload(vehicle2.current_load)
-print(str(vehicle1))
-print(vehicle2)
+    print(str(vehicle1))
+    print(vehicle2)
